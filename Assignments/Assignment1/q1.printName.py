fb4880dac318bb0cec42005edac33569c3346a7b
sagarmohanty2k00 <sagarmohanty2k00@gmail.com>
2021-01-23 12:52:40 +0530

    removed the python tutorials

--- Assignments/Assignment1/q1.printName.py
+++
@@ -1,4 +0,0 @@
-name = input("Write your name: ")
-
-for i in range(10):
-    print(name)
--- Assignments/Assignment1/q4.printOddBetweenMandN.py
+++
@@ -1,7 +0,0 @@
-m = int(input("enter m : "))
-n = int(input("enter n : "))
-
-while(m < n-1):
-    m += 1
-    if m % 2 == 1:
-        print(m)
